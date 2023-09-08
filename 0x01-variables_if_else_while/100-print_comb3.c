b5dcb64f168fa4a931c4ec0eae5b8de16f9c33a9
owisg <owisgamal.2020@gmail.com>
2023-09-08 12:25:19 -0700

    10 task

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entr point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+int num1, num2;
+for (num1 = 0; num1 <= 9; num1++)
+{
+for (num2 = num1 + 1; num2 <= 9; num2++)
+{
+putchar(num1 + '0');
+putchar(num2 + '0');
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
