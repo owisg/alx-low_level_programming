8d3b11e2ed11a0f5c6e4556bbc606184caafa241
owisg <owisgamal.2020@gmail.com>
2023-09-08 06:56:37 -0700

    2 task

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
-int main() {
-  char ch = 'a';
-while (ch <= 'z') {
+int main(void)
+{
+char ch = 'a';
+while (ch <= 'z')
+{
 putchar(ch);
 ch++;
-  }
+}
 putchar('\n');
 return 0;
 }
