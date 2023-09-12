d13c6336f21387607d45ee69f3ea4a751983e09f
owisg <owisgamal.2020@gmail.com>
2023-09-12 14:54:22 -0700

    last digit task

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -4,8 +4,14 @@
 * @n: the int to etract the last digit from
 * Return:  the value of the last digit
 */
-int print_last_digit(int n) {
-  int last_digit = n % 10;
-
-  return last_digit;
+int print_last_digit(int n)
+{
+int last_digit;
+if (n < 0)
+n = -n;
+last_digit = n % 10;
+if (last_digit < 0)
+last_digit = -last_digit;
+_putchar(last_digit + '0');
+return (last_digit);
 }
