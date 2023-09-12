16c47559cbcb82d40dcbf017500172d67032d354
owisg <owisgamal.2020@gmail.com>
2023-09-12 12:10:09 -0700

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,9 +2,10 @@
 /**
 * _ialpha - checks for alphabetic character
 * @c: is the char to be checked
-* Returns: 1 if c is a letter, 0 otherwise.
+* Returns: 1 if c is a letter, lowercase or uppercase
+* Returns: 0 otherwise.
 */
 int _isalpha(int c)
 {
-return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
+return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
