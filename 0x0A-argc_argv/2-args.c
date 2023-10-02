1d9c0ece6c54cc128a8663bc8e1bfb5efcb14622
owisg <owisgamal.2020@gmail.com>
2023-10-02 02:19:23 -0700

    args num

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main -  prints the number of arguments passed into it
+ * @argc: int
+ * @argv: list
+ * Return: 0
+ */
+#include <stdio.h>
+int main(int argc, char **argv)
+{
+int i;
+for (i = 1; i < argc; i++)
+{
+printf("%s\n", argv[i]);
+}
+return (0);
+}
