7a6888a74c5a0cfba505544a9d6ce423c7a3a918
owisg <owisgamal.2020@gmail.com>
2023-10-30 11:13:34 -0700

    task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,40 +1,50 @@
 #include "main.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
 
 /**
-*read_textfile - reads a text file and prints it to the Stdout
-*@filename: path od a filename
-*@letters: number to letters to read and write
-*Return: 0 if 'filename is NULL or cannot be open, read or write
-*or number of bytes used to call write
-*/
+ * read_textfile - Read a text file and print to STDOUT
+ * @filename: File name pointer
+ * @letters: Number of characters to read
+ * Return: 0 if file name is NULL
+ */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-FILE *file = fopen(filename, "r");
-char *buffer = malloc(letters);
-ssize_t bytes_read = fread(buffer, 1, letters, file);
+char *buf;
+int fd;
+ssize_t r, w;
+
 if (filename == NULL)
+return (0);
+buf = malloc(sizeof(char) * letters);
+if (buf == NULL)
+return (0);
+fd = open(filename, O_RDWR);
+if (fd == -1)
 {
+free(buf);
 return (0);
 }
-
-if (file == NULL)
+r = read(fd, buf, letters);
+if (r == -1)
 {
+free(buf);
+close(fd);
 return (0);
 }
-
-if (buffer == NULL)
+close(fd);
+w = write(STDOUT_FILENO, buf, r);
+if (w == -1)
 {
+free(buf);
 return (0);
 }
-
-if (bytes_read == 0)
-{
-free(buffer);
+if (w != r)
 return (0);
-}
-fwrite(buffer, 1, bytes_read, stdout);
-free(buffer);
-fclose(file);
-return (bytes_read);
+return (r);
 }
