b8d33eb4f988a3f4e633f3bab3229cd799aaeb52
owisg <owisgamal.2020@gmail.com>
2023-10-30 10:00:03 -0700

    task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,32 +1,36 @@
 #include "main.h"
 
+/**
+*read_textfile - reads a text file and prints it to the Stdout
+*@filename: path od a filename
+*@letters: number to letters to read and write
+*Return: 0 if 'filename is NULL or cannot be open, read or write
+*or number of bytes used to call write
+*/
 
-ssize_t read_textfile(const char *filename, size_t letters) {
+ssize_t read_textfile(const char *filename, size_t letters)
+{
 FILE *file = fopen(filename, "r");
 ssize_t bytes_read = 0;
-  char c;
-  if (filename == NULL) {
-    return 0;
-  }
+char c;
+if (filename == NULL)
+{
+return (0);
+}
 
-  if (file == NULL) {
-    return 0;
-  }
-
-
-  
-  while ((c = fgetc(file)) != EOF) {
-
-    if ((ssize_t)letters <= bytes_read) {
-      break;
-    }
-
-    _putchar(c);
-
-    bytes_read++;
-  }
-
-  fclose(file);
-
-  return bytes_read;
+if (file == NULL)
+{
+return (0);
+}
+while ((c = fgetc(file)) != EOF)
+{
+if ((ssize_t)letters <= bytes_read)
+{
+break;
+}
+_putchar(c);
+bytes_read++;
+}
+fclose(file);
+return (bytes_read);
 }
