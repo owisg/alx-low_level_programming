53775fa09769db008e6be6a773a1cddafd6651fa
owisg <owisgamal.2020@gmail.com>
2023-10-30 11:43:37 -0700

    task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,6 +1,12 @@
 #include "main.h"
 #include <string.h>
 
+/**
+*append_text_to_file - appends text at the end of a file.
+*@filename: file to append
+*@text_content: text to append
+*Return: 1 on success and -1 on failure
+*/
 
 int append_text_to_file(const char *filename, char *text_content)
 {
